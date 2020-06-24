dd717df684a16daefb29ace49a799ba83cafd039
jerry <chenjingxin1235@icloud.com>
2020-06-24 21:42:22 +0800

    Create MinorGCTest.java

---
+++ src/com/jerry/gcroot/MinorGCTest.java
@@ -0,0 +1,20 @@
+package com.jerry.gcroot;
+
+/**
+ * Vm args : -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8
+ */
+public class MinorGCTest {
+
+    private static final int _1MB = 1024 * 1024;
+
+    public static void main(String[] args) {
+        testAllocation();
+    }
+    public static void testAllocation(){
+        byte[] a1,a2,a3,a4;
+        a1 = new byte[2 * _1MB];
+        a2 = new byte[2 * _1MB];
+        a3 = new byte[2 * _1MB];
+        a4 = new byte[2 * _1MB];
+    }
+}
